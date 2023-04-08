af8f5a8754297ba367ed29a549b33f9bc13816ea
rashaibr <96967976+rashaibr@users.noreply.github.com>
2023-04-08 18:25:27 -0400

    slideshow

--- main.js
+++ main.js
@@ -198,3 +198,31 @@ function changeQuantity(key, quantity){
   }
   reloadCard();
 }
+
+///   SLIDESHOW    -start  --rasha ///
+
+// Get the slideshow container and images
+const slideshowContainer = document.querySelector('.slideshow-container');
+const images = slideshowContainer.querySelectorAll('img');
+
+// Set the first image to active
+let index = 0;
+images[index].classList.add('active');
+
+// Switch between images every 5 seconds
+setInterval(() => {
+  // Remove the active class from the current image
+  images[index].classList.remove('active');
+  
+  // Increment the index to switch to the next image
+  index++;
+  
+  // Reset the index to 0 if it exceeds the number of images
+  if (index >= images.length) {
+    index = 0;
+  }
+  
+  // Add the active class to the next image
+  images[index].classList.add('active');
+}, 5000);
+///  SLIDESHOW - END     ///
