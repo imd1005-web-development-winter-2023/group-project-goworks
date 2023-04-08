da8bea6077959af8bc9c4a55b5231df46ebd0bf8
Vaniya S <vaniyasharma2004@gmail.com>
2023-04-08 17:59:17 -0400

    added javascript for the cart

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 //
 
 // Constants
-const appID = "app";
+/*const appID = "app";
 const headingText = "Develop. Preview. Ship.";
 const headingTextIcon = "🚀";
 const projectDueDate = "11 April 2023 11:59";
@@ -70,4 +70,131 @@ function inititialise() {
 // Inits & Event Listeners
 //
 
-inititialise();
+inititialise();*/
+
+let openShopping=document.querySelector('.shopping');
+let closeShopping=document.querySelector('.closingShopping');
+let list=document.querySelector('.list');
+let listCard=document.querySelector('.listCart');
+let body=document.querySelector('.body');
+
+let total=document.querySelector('.total');
+let quantity=document.querySelector('.quantity');
+
+openShopping.addEventListener('click',()=>{
+  body.classList.add('active');
+})
+
+closeShopping.addEventListener('click',()=>{
+  body.classList.remove('active');
+})
+
+let products=[
+  {
+    id:1,
+    name:'Product name 1',
+    image:'1.PNG',
+    price:120000
+
+  },
+
+  {
+    id:2,
+    name:'Product name 2',
+    image:'2.PNG',
+    price:120000
+
+  },
+
+  {
+    id:3,
+    name:'Product name 3',
+    image:'3.PNG',
+    price:120000
+
+  },
+
+  {
+    id:4,
+    name:'Product name 4',
+    image:'4.PNG',
+    price:120000
+
+  },
+
+  {
+    id:5,
+    name:'Product name 5',
+    image:'5.PNG',
+    price:120000
+
+  },
+
+  {
+    id:6,
+    name:'Product name 6',
+    image:'6.PNG',
+    price:120000
+
+  },
+];
+
+let listCards=[];
+function initApp(){
+  products.forEach((value,key)=>{
+    let newDiv=document.createElement('div');
+    newDiv.classList.add('item');
+
+    newDiv.innerHTML=
+      <><img src="image/${value.image}" />
+      <div class="title">${value.name}</div>
+      <div class="price">${value.price.toLocaleString()}</div>
+      <button oneclick="addToCard(${hey})">Add To Card</button>
+    </>;
+
+      list.appendChild(newDiv);
+  })
+}
+initApp();
+
+function addToCard(key){
+  if(listCards[key] == null){
+      // copy product form list to list card
+      listCards[key] = JSON.parse(JSON.stringify(products[key]));
+      listCards[key].quantity = 1;
+  }
+  reloadCard();
+}
+function reloadCard(){
+  listCard.innerHTML = '';
+  let count = 0;
+  let totalPrice = 0;
+  listCards.forEach((value, key)=>{
+      totalPrice = totalPrice + value.price;
+      count = count + value.quantity;
+      if(value != null){
+          let newDiv = document.createElement('li');
+          newDiv.innerHTML = `
+              <div><img src="image/${value.image}"/></div>
+              <div>${value.name}</div>
+              <div>${value.price.toLocaleString()}</div>
+              <div>
+                  <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                  <div class="count">${value.quantity}</div>
+                  <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+              </div>`;
+              listCard.appendChild(newDiv);
+      }
+  })
+  total.innerText = totalPrice.toLocaleString();
+  quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+  if(quantity == 0){
+      delete listCards[key];
+  }else{
+      listCards[key].quantity = quantity;
+      listCards[key].price = quantity * products[key].price;
+  }
+  reloadCard();
+}
